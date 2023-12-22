b435c1e0130994f1ed1a9d799f5f7d884879b5f7
Ilya A. Shlyakhovoy <bulgarus@inbox.ru>
2023-12-23 00:05:12 +0300

    fix

--- src/RenderBlocks.tsx
+++ src/RenderBlocks.tsx
@@ -1,8 +1,7 @@
 import RenderBlockComponent from './RenderBlockComponent';
 
 export default ({content}:{ content:any[]}) => {
-  { 
-    // return [RenderBlockComponent(content[0])];
+  {  
     return content.map((el: any) => RenderBlockComponent(el));
   }
 };
